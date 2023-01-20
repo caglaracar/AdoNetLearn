c0c21484be2c1ef872e535d9220dbab08aaa3864
Caglar Acar <caglarracar@gmail.com>
2023-01-20 03:24:56 +0300

    last commit

---
+++ Ado.netExecuteReader/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.netExecuteReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Data Source=localhost; Initial Catalog=personel; User ID=sa; Password=1;";
+
+            #region parametresiz
+            SqlCommand command = new SqlCommand("select * from Customer", connection);
+
+            connection.Open();
+            SqlDataReader sqlDataReader = command.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                int customerId =(int)sqlDataReader[0];
+                string name =sqlDataReader["Name"].ToString();
+                string surname = sqlDataReader.GetString(2);
+            }
+            sqlDataReader.Close();
+            connection.Close();
+            #endregion
+
+            #region Parametreli
+            #endregion
+
+            #region Store Proc
+            #endregion
+        }
+    }
+}
--- Ado.netExecuteScaler/Program.cs
+++ Ado.netExecuteScaler/Program.cs
@@ -10,9 +10,31 @@ namespace Ado.netExecuteScaler
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {       
             SqlConnection connection = new SqlConnection();
             connection.ConnectionString = "Data Source=localhost; Initial Catalog=personel; User ID=sa; Password=1;";
+
+
+            #region parametresiz
+            //SqlCommand command = new SqlCommand("select Name from Customer where CustomerId=31", connection);
+            //connection.Open();
+            //var Name = command.ExecuteScalar();
+            //Console.WriteLine(Name);
+            //connection.Close();
+            #endregion
+
+            #region Parametreli
+            SqlCommand command2 = new SqlCommand("select Name from Customer where CustomerId=@Id", connection);
+            connection.Open();
+            command2.Parameters.Add("@Id", System.Data.SqlDbType.Int).Value = 1;
+            var Name2 = command2.ExecuteScalar();
+            Console.WriteLine(Name2);
+            connection.Close();
+            #endregion
+
+           
+
+
         }
     }
 }
